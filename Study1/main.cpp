8d2c64050c4a848b5eaece23cd2bcb3270f0d047
Johaerim <109541176+Johaerim@users.noreply.github.com>
2023-12-22 15:01:00 +0900

    Initial Commit

---
+++ Study1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Study1
+//
+//  Created by 조해림 on 12/22/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
